9fd874ff612dc8e9ed5413dcce5869b37da05be2
Gastón I. Silva <givanse@gmail.com>
2019-10-30 11:33:55 -0700

    better wording and layout

--- src/components/the-form/index.js
+++ src/components/the-form/index.js
@@ -61,56 +61,50 @@ export default class TheForm extends Component {
 							 onChange={e => this.updateBtcHodl(e)} />
 			</form>
 
-			<div class="col-50-50 text-center m-auto md:max-w-xl">
+			<div class="col-33-33-33 text-center m-auto md:max-w-xl">
 				<div>
 					₿ {f.sat(btcPerPerson)}
-					<br />
-					BTC per person
 				</div>
 				<div>
+				</div>
+				<div class="bg-blue-100">
 					₿ {f.sat(btcHodl)}
-					<br />
-					your Bitcoin 
 				</div>
 
 				<div>
 					1
-					<br />
-					person
 				</div>
+				<div class="">people</div>
 				<div>
 					{f.dec(btcHodlInIndividualShares(btcHodl))}
-					<br />
-					people
 				</div>
 
 
 				<div>
 					{f.dec(goldPerPersonKg * TROY_OUNCE)} oz
-					<br />
-					gold
 				</div>
+        <div class="">gold</div>
 				<div>
 					{f.dec(btcHodlInIndividualShares(btcHodl) * goldPerPersonKg * TROY_OUNCE)} oz
-					<br />
-					gold
 				</div>
 
 				<div>
 				  {f.dec(btcPerPerson / usaMillionaireMedianBroadPercentInBtc)}
-					<br />
-					total median millionaires
+				</div>
+			  <div class="">
+					millionaires
 				</div>
 				<div>
 				  {f.dec(btcHodl / usaMillionaireMedianBroadPercentInBtc)}
-					<br />
-					total median millionaires
 				</div>
 			</div>
 
 			<div class="text-center m-2">
-				BTC price {f.usd(btcPrice)}
+				Today's Bitcoin price
+				<br />
+				{f.usd(btcPrice)}
 			</div>
+
 			<div class="col-50-50 text-center m-auto md:max-w-xl">
 				<div>
 					{f.usd(btcPerPerson * btcPrice)}
@@ -126,98 +120,79 @@ export default class TheForm extends Component {
 
 			<h2>Everyone</h2>
 			<div class="text-center">
-			  world population {f.dec(worldPopulation)}
+			  The world population is {f.dec(worldPopulation)}.
+				<br />
+				That is seven billion seven hundred million.
 			</div>
 			<h3>Broad Money</h3>
-			<div class="col-50-50 text-center m-auto md:max-w-xl">
+			<div class="col-33-33-33 text-center m-auto md:max-w-xl">
 				<div>
-					{f.usd(moneySupply.broadMoney)}
-				  <br/>
-					Broad money supply
+					{f.usd(moneySupply.broadMoney)}<sup>*</sup>
 				</div>
+				<div>supply</div>
 				<div>
 					₿ {f.btc(btcRemainTSupply)}
-				  <br/>
-				  BTC supply
 				</div>
 
 				<div>
 					{f.usd(broadMoneyPerCapita)}
-					<br />
-					Broad money per capita
 				</div>
+				<div>per capita</div>
 				<div>
 					₿ {btcPerPerson.toFixed(8)}
-					<br />
-				  BTC per capita
 				</div>
 			</div>
 
       <h3>Gold</h3>
-			<div class="col-50-50 text-center m-auto md:max-w-xl">
+			<div class="col-33-33-33 text-center m-auto md:max-w-xl">
         <div>
 				  {f.dec(goldAboveGround)} kg
-				  <br/>
-				  gold above ground
         </div>
+				<div>supply</div>
 				<div>
 					₿ {f.btc(btcRemainTSupply)}
-				  <br/>
-				  BTC supply
 				</div>
 
         <div>
 					{(goldPerPersonKg).toFixed(3)} kg
 					&nbsp;or {(goldPerPersonKg * TROY_OUNCE).toFixed(3)} troy oz 
-					<br/>
-					gold per capita 
         </div>
+				<div>per capita</div>
 				<div>
 					₿ {btcPerPerson.toFixed(8)}
-					<br />
-				  BTC per capita
 				</div>
 			</div>
 
 		  <h2>Millionaire</h2>
-			<div class="col-50-50 text-center m-auto md:max-w-xl">
+			<div class="col-33-33-33 text-center m-auto md:max-w-xl">
 		    <div>
 					{f.usd(moneySupply.broadMoney)}
-				  <br/>
-					Broad money supply
 		    </div>
+				<div>supply</div>
 		    <div>
 					₿ {f.btc(btcRemainTSupply)}
-				  <br/>
-				  BTC supply
 		    </div>
 
 		    <div>
 					{f.per(usaMillionaireMedianBroadPercent)}
-					<br />
-					millionaire median net worth as a percentage of broad money
 		    </div>
+				<div>net worth</div>
 		    <div>
 					{f.per(usaMillionaireMedianBroadPercent)}
-					<br />
-					same percentage
 		    </div>
 
 		    <div>
-					{f.usd(usaMillionaireMedian)}
-					<br />
-					millionaire median net worth
+					{f.usd(usaMillionaireMedian)}<sup>*</sup>
 		    </div>
+				<div>net worth</div>
 		    <div>
 				  ₿ {f.btc(usaMillionaireMedianBroadPercentInBtc)}
-					<br />
-				  same level of wealth in BTC
 		    </div>
 			</div>	
 
  		  <h2>BTC Stats</h2>
 			<div class="text-center">
-				Total theoretical supply ₿ {f.btc(btcTCap)}
+				Theoretical total supply ₿ {f.btc(btcTCap)}
 				<br/>
 				Lost estimate ₿ {f.btc(btcLost)} ({btcLostPerc * 100}%)
 				<br />
@@ -226,6 +201,14 @@ export default class TheForm extends Component {
 				BTC per person ₿ {btcPerPerson.toFixed(8)}
 			</div>
 
+			<ul class="p-4 italic">
+				<li> 
+					* <a href="https://money.visualcapitalist.com/worlds-money-markets-one-visualization-2017/">
+						Desjardins, J. (2017, October 26). All of the World's Money and Markets in One Visualization. Retrieved October 30, 2019.
+				  </a>
+				</li>
+				<li>* millionaire net worth median</li>
+			</ul>
 			</div>
 		);
 	}
