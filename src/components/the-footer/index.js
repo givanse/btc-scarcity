26ed1118a6d2e58147fbe035eb977b5a466a3bc1
Gastón I. Silva <givanse@users.noreply.github.com>
2019-11-26 12:31:12 -0800

    .background-btc-orange

--- src/components/the-footer/index.js
+++ src/components/the-footer/index.js
@@ -6,7 +6,7 @@ export default class TheFooter extends Component {
   render() {
 
     return (
-      <footer>
+      <footer class="background-btc-orange">
         <a class="font-bold text-white text-2xl"
            href="https://bitcoin.org/en/faq#is-bitcoin-really-used-by-people">
           Is Bitcoin really used by people?
