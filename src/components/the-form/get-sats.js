e5fea2f826d66188b72fd52c3a94a7680142926c
Gastón I. Silva <givanse@users.noreply.github.com>
2019-12-17 08:25:46 -0800

    fix sats math

--- src/components/the-form/get-sats.js
+++ src/components/the-form/get-sats.js
@@ -1,13 +1,22 @@
 
 // 1 Bitcoin (BTC) = 100,000,000 Satoshis
 const ONE_HUNDRED_MILLION = 100000000;
+const ONE_SAT = 0.00000001;
 
 export default function getSats(amount) {
   amount = amount && amount !== Infinity ? amount : 0;
   const whole = amount >= 1 ? Number.parseInt(amount) : 0;
 
-  let sats = (amount - whole) * ONE_HUNDRED_MILLION;
-  sats = Math.round(sats);
+  let sats = (amount - whole);
+  if (sats < ONE_SAT) {
+    sats = 0;
+    return {btc: whole, sats};
+  }
+
+  sats = sats * ONE_HUNDRED_MILLION;
+  sats += Number.EPSILON;
+  sats = sats.toPrecision(8);
+  sats = Number.parseInt(sats);
 
   return {btc: whole, sats};
 }
