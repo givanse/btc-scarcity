e1165285755cbc4e18f29e0bcf2c45f98902631f
Gastón Silva <givanse@users.noreply.github.com>
2024-03-07 23:45:52 -0800

    redo btc section

--- src/components/bitcoin-section/index.js
+++ src/components/bitcoin-section/index.js
@@ -1,8 +1,8 @@
 import {h, Component } from 'preact';
 import f from '../../utils/formatter';
 import staticData from '../../utils/static-data';
+import Link from '../link';
 import toWords from '../the-form/to-words';
-import BtcSign from '../btc-sign';
 import ArrSlider from '../arr-slider';
 import style from './style';
 import { MarkupText, Text } from 'preact-i18n';
@@ -12,8 +12,6 @@ import { BTC_SLIDER_VALUES } from '../../utils/constants';
 import {
   btcToWords,
 } from '../../utils/words';
-
-const SAT_SIGN = ' sat';
 
 const {
   btcPerPerson,
@@ -40,14 +38,11 @@ export default class TheFooter extends Component {
     const {
       btcHodl,
       btcPrice,
-      goldPrice,
       onInputChange,
     } = this.props;
 
     const {btc, sats} = getSats(btcHodl); 
     const satsHodl = (btc * HUNDRED_M) + sats;
-
-    const proportionalToBTCGoldSharesOz = btcHodlInIndividualShares(btcHodl) * goldPerPersonOz;
 
     return (
       <div>
@@ -62,10 +57,6 @@ export default class TheFooter extends Component {
 
       <form class="text-center" onSubmit={e => e.preventDefault()}>
 
-        <p class="text-sm text-gray-700">
-          {toWords.btc(btcHodl)}
-        </p>
-
         <label for="btc-hodl" class="block w-0 h-0 overflow-hidden">
           bitcoin <Text id="amount">amount</Text>
         </label>
@@ -76,125 +67,77 @@ export default class TheFooter extends Component {
               placeholder="bitcoin amount"
               onChange={onInputChange} />
 
+        <p class="text-xs text-center italic text-gray-500 mt-2">
+          {btcToWords(btcHodl)}
+        </p>
+
+        <table class="text-sm text-gray-700 italic mb-4 mx-auto">
+          <tr>
+            <td class="text-right">
+              {f.btc(btcHodl)} *
+              <span class="price-synced-amount">
+                {f.usd(btcPrice)}
+              </span>
+              =&nbsp;&nbsp;
+            </td>
+
+            <td class="text-left">
+              <Link queryParams={`fiat=${(btcHodl * btcPrice).toFixed(2)}`} hash='cash'>
+                {f.usd(btcHodl * btcPrice)}
+              </Link>
+            </td>
+          </tr>
+        </table>
+
+        <table class={style["comparison"]}>
+          <tr>
+            <td>
+              <Text id="bitcoin.individual-world-share">
+                world share per individual
+              </Text>
+            </td>
+
+            <td>
+              {f.dec(btcHodlInIndividualShares(btcHodl))}
+              <i class="icon-person"></i>
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <Text id="millionaire-average">average millionaire</Text>
+            </td>
+
+            <td>
+              {f.decSmall(btcHodl / usaMillionaireAverageBroadPercentInBtc)}
+              <i class="icon-person"></i>
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <Text id="percenter-wealth">
+                a one percenter wealth
+              </Text>
+            </td>
+
+            <td>
+              {f.decSmall(btcHodl / netWorth1PercentMedianBroadMoneyPercentInBtc)}
+              <i class="icon-person"></i>
+            </td>
+          </tr>
+        </table>
+
         <ArrSlider name="btc-hodl-slider"
                    value={btcHodl}
                    values={BTC_SLIDER_VALUES}
-                   updateValue={this.props.onSliderChange} />
+                   updateValue={this.props.onSliderChange}>
+          {f.btc(BTC_SLIDER_VALUES[0])}
+          {f.btc(BTC_SLIDER_VALUES[BTC_SLIDER_VALUES.length - 1])}
+        </ArrSlider>
 
-        <p class="text-sm text-gray-700">
-          {btcToWords(btcHodl)}
-        </p>
       </form>
 
-      <table class={style["comparison"]}>
-
-        <tr>
-          <td class="text-xl">
-            1 <i class="icon-person"></i>
-            <br />
-            <span class="text-sm leading-none">
-              <MarkupText id="qty-per-person">quantity per person</MarkupText>
-            </span>
-          </td>
-          <td class="text-xl">
-            {f.dec(btcHodlInIndividualShares(btcHodl))} <i class="icon-person"></i>
-            <br />
-            <span class="text-sm">
-              <Text id="persons-per-qty">persons per quantity</Text>
-            </span>
-          </td>
-        </tr>
-
-        <tr class="bg-gray-300 text-sm">
-          <td colSpan="2">
-            bitcoin
-            <br/>
-            <span class="text-green-900">
-              {f.usd(btcPrice)}
-            </span>
-          </td>
-        </tr>
-
-        <tr>
-          <td>
-            <BtcSign /> {f.btc(btcPerPerson)}
-            <br />
-            <span class="price-synced-amount">
-              {f.usd(btcPerPerson * btcPrice)}
-            </span>
-          </td>
-          <td>
-            <BtcSign /> {f.btc(btcHodl)}
-            <br />
-            <span class="price-synced-amount">
-              {f.usd(btcHodl * btcPrice)}
-            </span>
-          </td>
-        </tr>
-
-        <tr class="bg-gray-300 text-sm">
-          <td colSpan="2">
-            <Text id="mined-gold">gold</Text>
-            <br/>
-            <span class="text-green-900">
-              {f.usd(goldPrice)}
-            </span>
-          </td>
-        </tr>
-
-        <tr>
-          <td>
-            {f.dec(goldPerPersonOz)} oz
-            <br />
-            <span class="price-synced-amount">
-              {f.usd(goldPerPersonOz * goldPrice)}
-            </span>
-          </td>
-          <td>
-            {f.dec(proportionalToBTCGoldSharesOz)} oz
-            <br />
-            <span class="price-synced-amount">
-              {f.usd(proportionalToBTCGoldSharesOz * goldPrice)}
-            </span>
-          </td>
-        </tr>
-
-        <tr class="bg-gray-300 text-sm">
-          <td colSpan="2">
-            <Text id="millionaire-average">average millionaire</Text>
-          </td>
-        </tr>
-
-        <tr>
-          <td>
-            {f.decSmall(btcPerPerson / usaMillionaireAverageBroadPercentInBtc)}
-            <i class="icon-person"></i>
-          </td>
-          <td>
-            {f.decSmall(btcHodl / usaMillionaireAverageBroadPercentInBtc)}
-            <i class="icon-person"></i>
-          </td>
-        </tr>
-
-        <tr class="bg-gray-300 text-sm">
-          <td colSpan="2">
-            <Text id="percenter-wealth">
-              a one percenter wealth
-            </Text>
-          </td>
-        </tr>
-
-        <tr>
-          <td>
-            {f.decSmall(btcPerPerson / netWorth1PercentMedianBroadMoneyPercentInBtc, 3)}
-            <i class="icon-person"></i>
-          </td>
-          <td>
-            {f.decSmall(btcHodl / netWorth1PercentMedianBroadMoneyPercentInBtc)}
-            <i class="icon-person"></i>
-          </td>
-        </tr>
-      </table>
       </div>
     );
   }
